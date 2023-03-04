75858c57863b507e62aaae571be16bc49d131709
TomTkacz <antomationtv@gmail.com>
2023-03-04 15:25:08 -0600

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='epic-by-tom-tkacz',
-    version='0.0.1',
+    version='0.0.2',
     descript='Say epic!',
     py_modules=["epic"],
     package_dir={'': 'src'},
